8040f1d3d53fa0fc6b85b568a9173e62e2bc5aba
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-12-15 01:55:32 +0200

    facebook login page

---
+++ app/src/main/java/com/example/routeandroiddiplomatasks/task1/FbloginPageSample.java
@@ -0,0 +1,16 @@
+package com.example.routeandroiddiplomatasks.task1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.routeandroiddiplomatasks.R;
+
+public class FbloginPageSample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fblogin_page_sample);
+    }
+}
