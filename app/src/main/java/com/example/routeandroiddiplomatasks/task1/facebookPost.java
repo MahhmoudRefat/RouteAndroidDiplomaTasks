d52e9dda26f71090e09274a61dc78548388449f1
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-12-15 04:00:06 +0200

    facebook post sample in xml design

---
+++ app/src/main/java/com/example/routeandroiddiplomatasks/task1/facebookPost.java
@@ -0,0 +1,16 @@
+package com.example.routeandroiddiplomatasks.task1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.routeandroiddiplomatasks.R;
+
+public class facebookPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_post);
+    }
+}
